f84a98376736fbc61afde6c97fd8fbacdf52744f
dklink <dougdklink3@gmail.com>
2020-05-17 15:56:10 -0400

    added skeleton of negative generation script

---
+++ data_utils/generate_negatives.py
@@ -0,0 +1,19 @@
+
+
+def generate_negatives():
+    """randomly generate images of NOT plastic from our dataset"""
+
+
+def is_valid(negative_bbox, positive_bboxes):
+    """boolean, determines whether the generated negative_bbox is a valid bounding box.
+    Criteria for validity
+        1. The negative bounding box overlaps with none of the positive bounding boxes
+        2. ??? any others you can think of
+    A bounding box is a list of 4 integers, [width, height, left, top]
+    :param negative_bbox: bounding box to test
+    :param positive_bboxes: list of bounding boxes against which to test negative_bbox
+    """
+
+    # YOUR CODE HERE :)
+
+    return False
