e95de22f98e5cc6155e3f53b923748c5de043d0a
Sean Roelofs <sean.forrester.roelofs@gmail.com>
2020-05-19 17:49:29 -0400

    Pytorch baby

---
+++ src/baseline_model.py
@@ -0,0 +1,33 @@
+from torch import nn
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+
+        self.cnn1 = nn.Conv2d(in_channels = 3, out_channels = 8, kernel_size = 3, stride = 1, padding = 1)
+        self.batchnorm1 = nn.BatchNorm2d(num_features = 8)
+        self.relu = nn.ReLU()
+        self.maxpool1 = nn.MaxPool2d(kernel_size = 2)
+        
+        self.cnn2 = nn.Conv2d(in_channels = 8, out_channels = 32, kernel_size = 3, stride = 1, padding = 1)
+        self.batchnorm2 = nn.BatchNorm2d(32)
+        self.maxpool2 = nn.MaxPool2d(kernel_size = 2)
+        
+        self.fc1 = nn.Linear(in_features = 32 * 16 ** 2, out_features = 1)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        out = self.cnn1(x)
+        out = self.batchnorm1(out)
+        out = self.relu(out)
+        out = self.maxpool1(out)
+        out = self.cnn2(out)
+        out = self.batchnorm2(out)
+        out = self.relu(out)
+        out = self.maxpool2(out)
+        out = out.view(-1, 32 * 16 ** 2)
+        out = self.fc1(out)
+        out = self.sigmoid(out)
+        return out
+
+
---
+++ src/train_baseline.py
@@ -0,0 +1,42 @@
+import time
+from baseline_model import CNN
+from util import dataset_loaders
+import torch.nn as nn
+import torch
+
+train_load, val_load, test_load = dataset_loaders()
+model = CNN()
+criterion = nn.BCELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr = 0.001)
+
+num_epochs = 2
+
+
+train_accuracy = []
+val_accuracy = []
+
+for epoch in range(num_epochs):
+    correct = 0
+    for i, (inputs, labels) in enumerate(train_load):
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        labels = labels.unsqueeze(1).type(torch.float)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        predicted = torch.round(outputs)
+        correct += (predicted.data == labels.data).sum().item()
+    train_accuracy.append(correct/len(train_load.dataset))
+
+    correct = 0
+    for i, (inputs, labels) in enumerate(val_load):
+        outputs = model(inputs)
+        predicted = torch.round(outputs)
+        labels = labels.unsqueeze(1).type(torch.float)
+        correct += (predicted.data == labels.data).sum().item()
+    val_accuracy.append(correct/len(val_load.dataset))
+    
+
+    print(train_accuracy[-1])
+    print(val_accuracy[-1])
+
